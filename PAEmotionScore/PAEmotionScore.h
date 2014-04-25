09382e50213aedf6fe99607a5c3edf55c07b1592
InnovationLab <innovationlab@iqsz-d0071.innovation.com>
2014-04-25 17:01:04 +0800

    Initial Commit

---
+++ PAEmotionScore/PAEmotionScore.h
@@ -0,0 +1,13 @@
+//
+//  PAEmotionScore.h
+//  PAEmotionScore
+//
+//  Created by InnovationLab on 14-4-25.
+//  Copyright (c) 2014年 pingantech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAEmotionScore : NSObject
+
+@end
---
+++ PAEmotionScore/PAEmotionScore.m
@@ -0,0 +1,13 @@
+//
+//  PAEmotionScore.m
+//  PAEmotionScore
+//
+//  Created by InnovationLab on 14-4-25.
+//  Copyright (c) 2014年 pingantech. All rights reserved.
+//
+
+#import "PAEmotionScore.h"
+
+@implementation PAEmotionScore
+
+@end
